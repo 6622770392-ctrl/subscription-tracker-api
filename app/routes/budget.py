a670e4721beb162ba8449eec2477710f3b36ac26
Justsaucz <6622772869@g.siit.tu.ac.th>
2026-02-11 20:57:49 +0700

    Update budget.py

--- app/routes/budget.py
+++ app/routes/budget.py
@@ -4,7 +4,7 @@ from app.models import Budget, Subscription, FrequencyType, StatusType
 
 bp = Blueprint('budget', __name__, url_prefix='/budget')
 
-@app.route('/<float:limit>', methods=["PUT"])
+@app.route('/<float:limit>', methods=['PUT'])
 def set_budget(limit):
 
     if limit <= 0:
